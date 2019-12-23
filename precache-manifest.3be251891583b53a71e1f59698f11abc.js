46c9cacaaff71c06d45e306f9e414e9025396e11
Ott Martens <ott.martens@pipedrive.com>
2019-12-23 15:03:33 +0200

    Updates

--- precache-manifest.3be251891583b53a71e1f59698f11abc.js
+++ precache-manifest.3be251891583b53a71e1f59698f11abc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83f7b237c9ddd66e038c31bb0217f196",
+    "revision": "b1dd3fadd7fec3e39f7e41bdd125432a",
     "url": "/ram-algorithms/index.html"
   },
   {
-    "revision": "fda37739c3f1e0397eb0",
-    "url": "/ram-algorithms/static/css/main.47121993.chunk.css"
+    "revision": "50118172174d3f0ee545",
+    "url": "/ram-algorithms/static/css/main.8b12125f.chunk.css"
   },
   {
     "revision": "a499c039526c1dacdfc7",
     "url": "/ram-algorithms/static/js/2.0c9434a2.chunk.js"
   },
   {
-    "revision": "fda37739c3f1e0397eb0",
-    "url": "/ram-algorithms/static/js/main.8a63adef.chunk.js"
+    "revision": "50118172174d3f0ee545",
+    "url": "/ram-algorithms/static/js/main.e52754a1.chunk.js"
   },
   {
     "revision": "cdaf414ab68bc0be5391",
